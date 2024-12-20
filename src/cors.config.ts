96f4e2818d951fe469d7214a0c1bc64000f3852a
Dmitr <dmitr@Marias-MacBook-Air.local>
2024-12-20 13:42:16 -0300

    add cors

--- src/cors.config.ts
+++ src/cors.config.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://awards-six.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
